ef36d413be3de3cb6bf8df4cdeddbfa3a9a699e6
casasvoley <acasaszan@gmail.com>
2023-07-18 01:51:09 +0200

    Update rod.py

--- src/rod.py
+++ src/rod.py
@@ -11,7 +11,7 @@ class Rod:
     Parameters:
         orientation (int): horizontal(0) or vertical(1)
         center_position_x (int): position of the center of the rod on the horizontal axis of the labyrinth 
-        center_position_y (int): position of the center of the rod on the vertical axis of the labyrinth 
+        center_position_y (int): position of the center of the rod on the vertical axis of the labyrinth
     '''  
 
     def __init__(self, orientation=HORIZONTAL, center_position_x=1, center_position_y=0):
