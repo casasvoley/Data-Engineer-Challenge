45702755cdd31315cce15caf47f44fbf64e873da
casasvoley <acasaszan@gmail.com>
2023-07-18 03:44:07 +0200

    Update labyrinth.py

--- src/labyrinth.py
+++ src/labyrinth.py
@@ -1,8 +1,10 @@
-from rod import Rod
-
 # Labyrinth cells constants
 EMPTY = '.'
 BLOCKED = '#'
+
+# Rod orientation constants
+HORIZONTAL = 0
+VERTICAL = 1
 
 class Labyrinth:
 
@@ -18,13 +20,13 @@ class Labyrinth:
 
     def __init__(self, labyrinth_map):
         self.labyrinth_map = labyrinth_map
-        self.rod = Rod()
 
-    def can_the_rod_move_to_the_right(self):
-        x = self.rod.center_position_x
-        y = self.rod.center_position_y
+    def can_the_rod_move_to_the_right(self, rod_state):
+        orientation = rod_state["rod_orientation"]
+        x = rod_state["rod_center_position_x"] 
+        y = rod_state["rod_center_position_y"]
 
-        if self.rod.is_horizontal():
+        if orientation == HORIZONTAL:
             # Check if the rod is not touching the right wall of the labyrinth and
             # if the cell on the right of the rod is empty
             if x+2 < len(self.labyrinth_map[0]) and self.labyrinth_map[y][x+2] == EMPTY:
@@ -32,7 +34,7 @@ class Labyrinth:
             else: 
                 return False
             
-        if self.rod.is_vertical():
+        if orientation == VERTICAL:
             # Check if the rod is not touching the right wall of the labyrinth and
             # if the three cells on the right of the rod are all empty
             if x+1 < len(self.labyrinth_map[0]) and not any(BLOCKED in a[x+1] for a in self.labyrinth_map[y-1:y+2]):
@@ -40,11 +42,12 @@ class Labyrinth:
             else: 
                 return False
             
-    def can_the_rod_move_to_the_left(self):
-        x = self.rod.center_position_x
-        y = self.rod.center_position_y
+    def can_the_rod_move_to_the_left(self, rod_state):
+        orientation = rod_state["rod_orientation"]
+        x = rod_state["rod_center_position_x"] 
+        y = rod_state["rod_center_position_y"]
 
-        if self.rod.is_horizontal():
+        if orientation == HORIZONTAL:
             # Check if the rod is not touching the left wall of the labyrinth and
             # if the cell on the left of the rod is empty
             if x-2 >= 0 and self.labyrinth_map[y][x-2] == EMPTY:
@@ -52,7 +55,7 @@ class Labyrinth:
             else: 
                 return False
             
-        if self.rod.is_vertical():
+        if orientation == VERTICAL:
             # Check if the rod is not touching the left wall of the labyrinth and
             # if the three cells on the left of the rod are all empty
             if x-1 >= 0 and not any(BLOCKED in a[x-1] for a in self.labyrinth_map[y-1:y+2]):
@@ -60,11 +63,12 @@ class Labyrinth:
             else: 
                 return False
             
-    def can_the_rod_move_upwards(self):
-        x = self.rod.center_position_x
-        y = self.rod.center_position_y
+    def can_the_rod_move_upwards(self, rod_state):
+        orientation = rod_state["rod_orientation"]
+        x = rod_state["rod_center_position_x"] 
+        y = rod_state["rod_center_position_y"]
 
-        if self.rod.is_horizontal():
+        if orientation == HORIZONTAL:
             # Check if the rod is not touching the top wall of the labyrinth and
             # if the three cells above the rod are all empty
             if y-1 >= 0 and not any(BLOCKED in a[x-1:x+2] for a in self.labyrinth_map[y-1]):
@@ -72,7 +76,7 @@ class Labyrinth:
             else: 
                 return False
             
-        if self.rod.is_vertical():
+        if orientation == VERTICAL:
             # Check if the rod is not touching the top wall of the labyrinth and
             # if the cell above the rod is empty
             if y-2 >= 0 and self.labyrinth_map[y-2][x] == EMPTY:
@@ -80,11 +84,12 @@ class Labyrinth:
             else: 
                 return False
             
-    def can_the_rod_move_downwards(self):
-        x = self.rod.center_position_x
-        y = self.rod.center_position_y
+    def can_the_rod_move_downwards(self, rod_state):
+        orientation = rod_state["rod_orientation"]
+        x = rod_state["rod_center_position_x"] 
+        y = rod_state["rod_center_position_y"]
 
-        if self.rod.is_horizontal():
+        if orientation == HORIZONTAL:
             # Check if the rod is not touching the bottom wall of the labyrinth and
             # if the three cells below the rod are all empty
             if y+1 < len(self.labyrinth_map) and not any(BLOCKED in a[x-1:x+2] for a in self.labyrinth_map[y+1]):
@@ -92,7 +97,7 @@ class Labyrinth:
             else: 
                 return False
             
-        if self.rod.is_vertical():
+        if orientation == VERTICAL:
             # Check if the rod is not touching the bottom wall of the labyrinth and
             # if the cell below the rod is empty
             if y+2 < len(self.labyrinth_map) and self.labyrinth_map[y+2][x] == EMPTY:
@@ -100,9 +105,10 @@ class Labyrinth:
             else: 
                 return False
             
-    def can_the_rod_change_orientation(self):
-        x = self.rod.center_position_x
-        y = self.rod.center_position_y
+    def can_the_rod_change_orientation(self, rod_state):
+        orientation = rod_state["rod_orientation"]
+        x = rod_state["rod_center_position_x"] 
+        y = rod_state["rod_center_position_y"]
 
         # Check if the rod is far away enough from the boundaries to rotate and 
         # if the cells in the 3x3 area surrounding it are all empty
