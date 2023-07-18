a5f7bc4188783d4514ace60e403d829732fcb3fc
casasvoley <acasaszan@gmail.com>
2023-07-18 20:20:51 +0200

    Tests scripted and passed

---
+++ tests/test_03.py
@@ -0,0 +1,21 @@
+from src.labyrinth import Labyrinth
+from src.planner import find_minimal_number_of_moves
+
+layout = [['.', '.', '.'], 
+          ['.', '.', '.'], 
+          ['.', '.', '.']]
+
+initial_state = {
+    "rod_orientation": 0,
+    "rod_center_position_x": 1,
+    "rod_center_position_y": 0
+    }
+
+goal = [len(layout[0])-1, len(layout)-1]
+
+labyrinth = Labyrinth(layout)
+
+number_of_moves = find_minimal_number_of_moves(labyrinth=labyrinth, initial_state=initial_state, goal=goal)
+
+def test_answer():
+    assert number_of_moves == 2
